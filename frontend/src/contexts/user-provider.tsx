fe5d0aaf99f756e8302a5324d0b7d36b54cd5fd4
asfalfa <glkin9@gmail.com>
2024-04-29 17:33:23 +0200

    feat(Frontend): :sparkles: Added User Provider

---
+++ frontend/src/contexts/user-provider.tsx
@@ -0,0 +1,58 @@
+import { createContext, useState, useEffect, useContext, PropsWithChildren } from 'react';
+import Cookies  from 'js-cookie';
+
+interface JWTUser {
+    id: string; 
+    email: string; 
+    username: string; 
+    avatar: string; 
+    role: string;
+}
+
+type UserProviderState = {
+    user?: JWTUser,
+}
+
+const UserContext = createContext<UserProviderState>({});
+
+export function UserProvider({ children }: PropsWithChildren) {
+    const [user, setUser] = useState<JWTUser | undefined>();
+    const accessToken = Cookies.get('__catalyst__jwt');
+    
+    const getUserInfo = async(accessToken: string): Promise<JWTUser | void> => {
+        try {
+            const res = await fetch(`${import.meta.env.VITE_BACKEND_ORIGIN}/users/info`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            });
+            setUser(await res.json())
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }
+
+    useEffect((): void => {
+        if(accessToken && !user){
+            getUserInfo(accessToken);
+        }
+    }, [accessToken, user]);
+
+    return (
+        <UserContext.Provider value={({ user })}>
+          {children}
+        </UserContext.Provider>
+      )
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext)
+    
+    if (context === undefined)
+        throw new Error("useUser must be used within a UserProvider")
+    
+    return context
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,19 +6,29 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './routes/home';
-import { ThemeProvider } from './contexts/theme-provider';
+import { ThemeProvider } from '@/contexts/theme-provider';
+import { UserProvider } from '@/contexts/user-provider';
+import Navmenu from './components/ui/navmenu';
+import JWTGet from './routes/JWTGet';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/jwt",
+    element: <JWTGet />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <UserProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="catalyst-theme">
+        <Navmenu />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </UserProvider>
   </React.StrictMode>,
 )
