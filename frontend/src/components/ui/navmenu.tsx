9646afc2c353276c313f5d60ae643fd31276a3f0
asfalfa <glkin9@gmail.com>
2024-04-29 17:34:58 +0200

    feat(Frontend): :sparkles: Basic navmenu with theme toggler and user signin/settings

---
+++ frontend/src/components/ui/navmenu.tsx
@@ -0,0 +1,59 @@
+import { ModeToggle } from "@/components/mode-toggle"
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { SignInForm } from "../user/signin-form"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+import { UserRound } from "lucide-react"
+import { useUser } from "@/contexts/user-provider"
+
+export default function Navmenu(){
+    const { user } = useUser()
+    console.log(user)
+    return(
+        <>
+        <header>
+            <div className="w-fit m-auto flex p-4">
+                <ModeToggle/>
+                <Dialog>
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <DialogTrigger asChild>
+                                    <Button variant="outline" size="icon">
+                                        {user ?
+                                            <Avatar className="rounded-md hover:contrast-50 p-[0.2rem]">
+                                                <AvatarImage className="rounded-sm" src={user.avatar} />
+                                                <AvatarFallback>{user.username[0]}</AvatarFallback>
+                                            </Avatar>
+                                        :
+                                        <UserRound />}
+                                    </Button>
+                                </DialogTrigger>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                            {user ? 
+                            <p>Settings</p>
+                            :
+                            <p>Sign In</p>}
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                    <DialogContent>
+                        <SignInForm />
+                    </DialogContent>
+                </Dialog>
+            </div>
+        </header>
+        </>
+    )
+}
