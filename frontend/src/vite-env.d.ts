1568206ea89f30bb14bad5ae5316491306521090
asfalfa <glkin9@gmail.com>
2024-04-30 19:35:47 +0200

    feat(Frontend): :sparkles: Add models for projects and users

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,38 @@
 /// <reference types="vite/client" />
+
+interface User {
+    id: string;
+    email: string;
+    username: string;
+    nickname: string;
+    avatar: string;
+    role: string;
+}
+
+interface Post {
+    id: string
+    content: string;
+    createdAt: Date;
+    updatedAt: Date;
+    author: User;
+}
+interface Branch {
+    id: string
+    name: string;
+    description: string;
+    createdAt: Date;
+    updatedAt: Date;
+    author: User;
+    default: boolean;
+    posts: Post[];
+}
+interface Project {
+    id: string
+    name: string;
+    description: string;
+    avatar: string;
+    createdAt: Date;
+    updatedAt: Date;
+    author: User;
+    branches: Branch[];
+}
