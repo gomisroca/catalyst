34b31ece5e2be2df41c5c3c3d9bc95164d1e9924
asfalfa <glkin9@gmail.com>
2024-05-04 16:17:01 +0200

    feat(DB): :sparkles: Added followedBy to user model

---
+++ backend/prisma/migrations/20240504114812_user_follows/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Follows` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Follows" DROP CONSTRAINT "Follows_followedById_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Follows" DROP CONSTRAINT "Follows_followingId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "following" TEXT[];
+
+-- DropTable
+DROP TABLE "Follows";
