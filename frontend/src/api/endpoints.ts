a4de6e2326b7104ebd3e84f82a91346defef6885
asfalfa <glkin9@gmail.com>
2025-01-07 12:31:04 +0100

    feat: introduce project endpoints

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -16,4 +16,25 @@ export const ENDPOINTS = {
   INTERACTIONS: {
     FOLLOW: (id: string) => `/interactions/user/${id}/follow`,
   },
+  PROJECTS: {
+    LIST: '/projects',
+    DETAIL: (id: string) => `/projects/${id}`,
+    CREATE: '/projects',
+    UPDATE: (id: string) => `/projects/${id}`,
+    DELETE: (id: string) => `/projects/${id}`,
+  },
+  BRANCHES: {
+    LIST: '/branches',
+    DETAIL: (id: string) => `/branches/${id}`,
+    CREATE: '/branches',
+    UPDATE: '/branches',
+    DELETE: '/branches',
+  },
+  POSTS: {
+    LIST: '/posts',
+    DETAIL: (id: string) => `/posts/${id}`,
+    CREATE: '/posts',
+    UPDATE: '/posts',
+    DELETE: '/posts',
+  },
 };
