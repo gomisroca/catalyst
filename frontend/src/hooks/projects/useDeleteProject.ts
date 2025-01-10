9f44a8317640585178f1f44d713ed92ed9870aa4
asfalfa <glkin9@gmail.com>
2025-01-10 18:07:23 +0100

    fix: proper name function

--- frontend/src/hooks/projects/useDeleteProject.ts
+++ frontend/src/hooks/projects/useDeleteProject.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { projectService } from '@/api/services/projectService';
 
-export const useDeleteUser = () => {
+export const useDeleteProject = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
