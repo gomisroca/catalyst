65559120e2ca5cf95b1ba2394d71fb46d8ea54a7
asfalfa <glkin9@gmail.com>
2025-01-21 10:01:25 +0100

    fix: simplify navmenu

--- frontend/src/components/ui/navmenu.tsx
+++ frontend/src/components/ui/navmenu.tsx
@@ -11,33 +11,31 @@ import { SignInForm } from '@/components/user/signin-form';
 import { UserSettingsForm } from '@/components/user/user-settings-form';
 import UserMenuButton from '@/components/user/user-menu-button';
 import SignInButton from '@/components/user/signin-button';
-import { useNavigate } from 'react-router-dom';
 
 export default function Navmenu() {
-  const navigate = useNavigate();
   const { data: user } = useGetSelf();
-
-  const handleClick = (href: string) => {
-    navigate(href);
-  };
 
   return (
     <>
       <header>
         <div className="m-auto flex w-fit gap-1 p-4">
-          <Button className="w-[40px]" size="icon" variant={'outline'}>
-            <span className="sr-only">Home</span>
-            <Home className="w-[40px]" onClick={() => handleClick('/')} />
-          </Button>
+          <a href="/">
+            <Button className="w-[40px]" size="icon" variant={'outline'}>
+              <span className="sr-only">Home</span>
+              <Home className="w-[40px]" />
+            </Button>
+          </a>
           <ModeToggle />
           <Dialog>
             {user ? <UserMenuButton user={user} /> : <SignInButton />}
             <DialogContent className="w-5/6 rounded-md">{user ? <UserSettingsForm /> : <SignInForm />}</DialogContent>
           </Dialog>
           {user && (
-            <Button size="icon" variant={'outline'} onClick={() => navigate('/new')}>
-              <FiFolderPlus />
-            </Button>
+            <a href="/new">
+              <Button size="icon" variant={'outline'}>
+                <FiFolderPlus />
+              </Button>
+            </a>
           )}
         </div>
       </header>
