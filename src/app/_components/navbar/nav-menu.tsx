2bfe6174977b587335dcc4abeeb00a8d7c2a658a
asfalfa <glkin9@gmail.com>
2025-02-02 13:04:48 +0100

    style: navmenu dropdown

--- src/app/_components/navbar/nav-menu.tsx
+++ src/app/_components/navbar/nav-menu.tsx
@@ -3,19 +3,15 @@
 import { useEffect, useRef, useState } from 'react';
 import Button from '@/app/_components/ui/button';
 import { MdOutlineMenu, MdClear } from 'react-icons/md';
-import { twMerge } from 'tailwind-merge';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 function MenuToggle({ open, setOpen }: { open: boolean; setOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
   return <Button onClick={() => setOpen(!open)}>{open ? <MdClear size={20} /> : <MdOutlineMenu size={20} />}</Button>;
 }
 
-function Menu({ open }: { open: boolean }) {
+function Menu() {
   return (
-    <div
-      className={twMerge(
-        'absolute bottom-0 right-10 top-0 hidden items-center justify-center rounded-full px-4 transition duration-200 ease-in-out',
-        open && 'flex'
-      )}>
+    <div className="absolute bottom-0 right-0 top-[0.75rem] flex min-h-16 items-center justify-center rounded-md bg-zinc-900 p-4">
       hello
     </div>
   );
@@ -23,6 +19,7 @@ function Menu({ open }: { open: boolean }) {
 
 function NavMenu() {
   const [open, setOpen] = useState(false);
+  const [parent] = useAutoAnimate();
   const menuRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -31,29 +28,21 @@ function NavMenu() {
         setOpen(false);
       }
     }
-
-    // Add event listener when menu is open
     if (open) {
       document.addEventListener('mousedown', handleClickOutside);
     }
 
-    // Cleanup function to remove event listener
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [open]);
 
   return (
-    <div
-      ref={menuRef}
-      className={twMerge(
-        'relative',
-        open &&
-          'flex w-64 justify-end rounded-full border border-zinc-300 bg-zinc-200 transition duration-200 ease-in-out dark:border-zinc-700 dark:bg-zinc-800'
-      )}>
-      {open && <Menu open={open} />}
-
+    <div ref={menuRef}>
       <MenuToggle open={open} setOpen={setOpen} />
+      <div ref={parent} className="relative">
+        {open && <Menu />}
+      </div>
     </div>
   );
 }
