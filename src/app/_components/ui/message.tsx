9746fd31fdf7627772785474cc6b620161c3e079
asfalfa <glkin9@gmail.com>
2025-02-02 12:05:45 +0100

    fix: adjust message popover

---
+++ src/app/_components/ui/message.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { messageAtom } from '@/atoms/message';
+import { useAtom } from 'jotai';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+
+const Message = () => {
+  const [{ message, error }, setMessage] = useAtom(messageAtom);
+
+  const pathname = usePathname(); // Get the current path
+
+  // Clear message and error on route change
+  useEffect(() => {
+    if (message || error) {
+      setMessage({ message: null });
+    }
+  }, [pathname]);
+
+  // Automatically hide popup after 5 seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setMessage({ message: null });
+    }, 5000);
+
+    // Cleanup timeout when popup state changes
+    return () => clearTimeout(timeout);
+  }, [message]);
+
+  if (!message) return null;
+
+  return (
+    <div
+      className={`fixed bottom-10 left-0 right-0 z-[99] m-auto flex w-[90vw] flex-col items-center justify-center gap-2 rounded-lg border px-5 py-2 font-semibold xl:w-[30vw] ${
+        error
+          ? 'border-red-500 bg-red-200/90 dark:bg-red-800/90'
+          : 'border-green-500 bg-green-200/90 dark:bg-green-800/90'
+      }`}>
+      {/* Conditional Text Based on Error */}
+      <p>{error ? 'An error occurred' : 'Success'}</p>
+      <h1 className="text-xl">{message}</h1>
+      {error && <p>Please try again</p>}
+    </div>
+  );
+};
+
+export default Message;
