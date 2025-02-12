db4553079f7daf07eaa97b5a8199b21f2dde0c27
asfalfa <glkin9@gmail.com>
2025-02-12 22:41:48 +0100

    fix: adjust config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,21 @@ const config = {
   },
   "plugins": [
     "@typescript-eslint",
-    "drizzle"
+    "drizzle",
+    "prettier",
+    "vitest-globals"
   ],
   "extends": [
     "next/core-web-vitals",
+    "plugin:prettier/recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:vitest-globals/recommended",
+    "plugin:storybook/recommended"
   ],
   "rules": {
+    "prettier/prettier": "error",
+    "no-console": "warn",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
