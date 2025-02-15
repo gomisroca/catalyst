adb603ce54f2d2cafe4d15ba4fcf4042fb9ab74c
asfalfa <glkin9@gmail.com>
2025-02-15 11:46:47 +0100

    feat: init basic magic link sign in with server actions

--- src/app/@modal/(.)sign-in/page.tsx
+++ src/app/@modal/(.)sign-in/page.tsx
@@ -1,17 +1,10 @@
-'use client';
-
-import Button from '@/app/_components/ui/button';
 import Modal from '@/app/_components/ui/modal';
+import { SignInForm } from '@/app/sign-in/page';
 
 export default function SignInModal() {
   return (
     <Modal>
-      <div className="flex flex-col items-center justify-center gap-4">
-        <p className="text-center text-2xl">Sign in to continue</p>
-        <Button className="w-full" onClick={() => alert('Sign in')}>
-          Sign in
-        </Button>
-      </div>
+      <SignInForm />
     </Modal>
   );
 }
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,7 +1,32 @@
-export default async function SignIn() {
+import { signIn } from '@/server/auth';
+import Form from 'next/form';
+import SubmitButton from '../_components/submit-button';
+
+function EmailForm() {
   return (
-    <div>
-      <h1>Sign in!</h1>
+    <Form
+      action={async (formData: FormData) => {
+        'use server';
+        const email = formData.get('email');
+        if (!email || typeof email !== 'string' || email.trim() === '' || !email.includes('@')) {
+          return;
+        }
+        await signIn('nodemailer', { redirect: false, email });
+      }}>
+      <input type="email" name="email" placeholder="Email" required />
+      <SubmitButton baseText="Sign In" pendingText="Signing In..." />
+    </Form>
+  );
+}
+
+export function SignInForm() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <EmailForm />
     </div>
   );
+}
+
+export default async function SignIn() {
+  return <SignInForm />;
 }
