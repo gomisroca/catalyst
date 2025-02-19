9028ae626389d7271e66512275c179bc737bad48
asfalfa <glkin9@gmail.com>
2025-02-19 22:26:03 +0100

    feat: add permission check to project fetch

--- src/server/queries/projects.ts
+++ src/server/queries/projects.ts
@@ -7,12 +7,12 @@ import { projects } from '../db/schema';
 export async function getProject(id: string) {
   // Ensure branch exists, and user has access to it
   const session = await auth();
-  // const projectPermissions = await db.query.projectsPermissions.findFirst({
-  //   where: (permissions, { eq }) => eq(permissions.projectId, id),
-  // });
-  // if (!projectPermissions) throw new Error('Project with the given ID does not exist');
-  // if (projectPermissions.private && (!session?.user.id || !projectPermissions.allowedUsers.includes(session?.user.id)))
-  //   throw new Error('Unauthorized');
+  const projectPermissions = await db.query.projectsPermissions.findFirst({
+    where: (permissions, { eq }) => eq(permissions.projectId, id),
+  });
+  if (!projectPermissions) throw new Error('Project with the given ID does not exist');
+  if (projectPermissions.private && (!session?.user.id || !projectPermissions.allowedUsers.includes(session?.user.id)))
+    throw new Error('Unauthorized');
 
   // If all checks pass, return project
   const project = await db.select().from(projects).where(eq(projects.id, id));
