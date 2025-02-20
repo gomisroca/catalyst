6749e6d6fdb12302ccc663cc208591995b1f484b
asfalfa <glkin9@gmail.com>
2025-02-20 14:34:31 +0100

    fix: adjust project creation modal to use actual form

---
+++ src/app/@modal/(.)projects/create/page.tsx
@@ -0,0 +1,10 @@
+import Modal from '@/app/_components/ui/modal';
+import CreateProjectForm from '@/app/projects/create/page';
+
+export default function CreateProjectModal() {
+  return (
+    <Modal>
+      <CreateProjectForm />
+    </Modal>
+  );
+}
