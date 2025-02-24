e2e4467815e4d77b0c5c6ca9ac2f7438cbcdb0f4
asfalfa <glkin9@gmail.com>
2025-02-24 13:01:19 +0100

    feat: basic shape branch view

--- src/app/projects/[projectId]/(branches)/[branchId]/page.tsx
+++ src/app/projects/[projectId]/(branches)/[branchId]/page.tsx
@@ -2,6 +2,9 @@ import { getBranch } from '@/server/queries/branches';
 import PostList from './post-list';
 import { auth } from '@/server/auth';
 import Link from '@/app/_components/ui/link';
+import { FaCodeBranch, FaPen } from 'react-icons/fa6';
+import ExpandedDescription from '@/app/_components/projects/ExpandedDescription';
+import BranchInteractions from './(interactions)/branch-interactions';
 
 export default async function BranchPage({ params }: { params: Promise<{ projectId: string; branchId: string }> }) {
   const session = await auth();
@@ -13,14 +16,37 @@ export default async function BranchPage({ params }: { params: Promise<{ project
     (session && data.permissions?.allowedUsers?.includes(session?.user?.id));
 
   return (
-    <main>
-      <header>
-        <h1>{data.name}</h1>
-        Here will be a description of the branch, maybe a header section with some details like date of creation, number
-        of contributions, interactions, etc
+    <main className="flex w-full flex-col gap-4">
+      <header className="border-b border-zinc-300 pb-2 dark:border-zinc-700">
+        <section className="flex items-center justify-between gap-2">
+          <div className="flex items-center gap-2">
+            <FaCodeBranch size={12} />
+            <h1 className="text-lg font-bold">{data.name}</h1>
+          </div>
+          <div className="flex gap-1 text-sm font-medium text-zinc-500 dark:text-zinc-400">
+            <span>{data.author}</span>
+            <span>•</span>
+            <span>
+              {data.createdAt.toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </span>
+          </div>
+        </section>
+        {data.description && <ExpandedDescription description={data.description} />}
+        <BranchInteractions branchId={data.id} />
       </header>
       <PostList branchId={data.id} />
-      {allowCollaborate && <Link href={`/projects/${data.projectId}/${data.id}/create`}>Post</Link>}
+      {allowCollaborate && (
+        <Link
+          href={`/projects/${data.projectId}/${data.id}/create`}
+          className="mx-auto flex w-1/3 items-center justify-center gap-2 font-semibold tracking-tight uppercase">
+          <FaPen size={12} />
+          <span>Post</span>
+        </Link>
+      )}
     </main>
   );
 }
