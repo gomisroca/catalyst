98edf40ec3504768a618adfa9d4336aaa1143daf
asfalfa <glkin9@gmail.com>
2025-02-25 10:22:35 +0100

    feat: branch creation form as modal

---
+++ src/app/@modal/(.)projects/[projectId]/create/page.tsx
@@ -0,0 +1,10 @@
+import Modal from '@/app/_components/ui/modal';
+import CreateBranchForm from '@/app/projects/[projectId]/(branches)/create/page';
+
+export default function CreateBranchModal() {
+  return (
+    <Modal>
+      <CreateBranchForm />
+    </Modal>
+  );
+}
