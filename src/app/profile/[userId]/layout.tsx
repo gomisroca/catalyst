312c11c0291e8d23a61871b8d6329abce1526052
asfalfa <glkin9@gmail.com>
2025-03-04 18:17:02 +0100

    style: restructure profile header slightly

--- src/app/profile/[userId]/layout.tsx
+++ src/app/profile/[userId]/layout.tsx
@@ -15,24 +15,25 @@ export default async function ProfileLayout({
   const session = await auth();
 
   const data = await getUserProfile((await params).userId);
-
   if (!data) return null;
 
   return (
     <div className="flex w-sm flex-col items-center justify-start gap-4 rounded-lg bg-zinc-100 p-4 drop-shadow-md md:w-xl lg:w-2xl dark:bg-zinc-900">
       <header className="flex w-full items-center justify-between border-b border-zinc-300 pb-2 dark:border-zinc-700">
         <div className="flex items-center gap-2">
-          {data.image && (
-            <span className="items-center justify-center rounded-full bg-radial-[at_15%_15%] via-zinc-300 to-75% p-1 dark:via-zinc-700">
-              <Image src={data.image} alt="Project Picture" width={50} height={50} className="h-12 w-12 rounded-full" />
-            </span>
-          )}
-          <section className="flex flex-col items-start justify-start">
-            <h1 className="text-2xl leading-tight font-bold tracking-tight">{data.name ?? data.email.split('@')[0]}</h1>
-          </section>
+          <span className="items-center justify-center rounded-full bg-radial-[at_15%_15%] via-zinc-300 to-75% p-1 dark:via-zinc-700">
+            <Image
+              src={data.image ?? '/user.jpg'}
+              alt="Profile Picture"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+          </span>
+          <h1 className="text-2xl leading-tight font-bold tracking-tight">{data.name ?? data.email.split('@')[0]}</h1>
         </div>
+        <FollowMenu session={session} followers={data.followers} />
       </header>
-      <FollowMenu session={session} followers={data.followers} />
       {/* 
       We will have various children views
       To see what the user has posted,
