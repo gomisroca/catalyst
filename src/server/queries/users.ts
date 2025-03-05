c68f9e90b3cc50e9a52fc7489a4d1a085b0f1356
asfalfa <glkin9@gmail.com>
2025-03-05 12:04:52 +0100

    fix: divide user queries by content

--- src/server/queries/users.ts
+++ src/server/queries/users.ts
@@ -1,45 +1,22 @@
 import 'server-only';
 import { db } from '../db';
 import { eq, sql } from 'drizzle-orm';
-import { branches, follows, posts, projects, users } from '../db/schema';
+import {
+  branches as branchesSchema,
+  follows,
+  postsMedia,
+  posts as postsSchema,
+  projects as projectsSchema,
+  users,
+} from '../db/schema';
 
 export async function getUserProfile(userId: string) {
   const data = await db
     .select({
       user: users,
-      projects: sql<Array<{ id: string; name: string; updatedAt: string }>>`
-      json_agg(
-        DISTINCT jsonb_build_object(
-          'id', ${projects.id},
-          'name', ${projects.name},
-          'updatedAt', ${projects.updatedAt}
-        )
-      )
-    `.as('projects'),
-      branches: sql<Array<{ id: string; name: string; updatedAt: string }>>`
-      json_agg(
-        DISTINCT jsonb_build_object(
-          'id', ${branches.id},
-          'name', ${branches.name},
-          'updatedAt', ${branches.updatedAt}
-        )
-      )
-      `.as('branches'),
-      posts: sql<Array<{ id: string; name: string; updatedAt: string }>>`
-      json_agg(
-        DISTINCT jsonb_build_object(
-          'id', ${posts.id},
-          'title', ${posts.title},
-          'updatedAt', ${posts.updatedAt}
-        )
-      )
-      `.as('posts'),
     })
     .from(users)
     .where(eq(users.id, userId))
-    .leftJoin(projects, eq(projects.authorId, userId))
-    .leftJoin(branches, eq(branches.authorId, userId))
-    .leftJoin(posts, eq(posts.authorId, userId))
     .groupBy(users.id);
 
   if (!data[0]) throw new Error('User with the given ID does not exist');
@@ -61,12 +38,74 @@ export async function getUserProfile(userId: string) {
 
   return {
     ...data[0].user,
-    projects: data[0].projects,
-    branches: data[0].branches,
-    posts: data[0].posts,
     followers: followersData.map((user) => ({
       ...user,
       name: user.name ?? user.email?.split('@')[0],
+    })),
+  };
+}
+
+export async function getUserContributions(userId: string) {
+  const [projects, branches, posts] = await Promise.all([
+    await db
+      .select()
+      .from(projectsSchema)
+      .where(eq(projectsSchema.authorId, userId))
+      .orderBy(sql`${projectsSchema.createdAt} DESC`),
+    await db
+      .select({
+        branch: branchesSchema,
+        project: projectsSchema,
+      })
+      .from(branchesSchema)
+      .where(eq(branchesSchema.authorId, userId))
+      .leftJoin(projectsSchema, eq(branchesSchema.projectId, projectsSchema.id))
+      .orderBy(sql`${branchesSchema.createdAt} DESC`),
+    await db
+      .select({
+        post: postsSchema,
+        media: sql<Array<{ id: string; name: string; url: string }>>`
+        COALESCE(
+          json_agg(
+            json_build_object(
+              'id', ${postsMedia.id},
+              'name', ${postsMedia.name},
+              'url', ${postsMedia.url}
+            )
+          ) FILTER (WHERE ${postsMedia.id} IS NOT NULL),
+          '[]'::json
+        )
+      `.as('media'),
+      })
+      .from(postsSchema)
+      .where(eq(postsSchema.authorId, userId))
+      .leftJoin(postsMedia, eq(postsSchema.id, postsMedia.postId))
+      .groupBy(postsSchema.id),
+  ]);
+
+  return {
+    projects: projects.map((project) => ({
+      id: project.id,
+      name: project.name,
+      description: project.description,
+      picture: project.picture,
+      updatedAt: project.updatedAt ?? project.createdAt,
+    })),
+    branches: branches.map((data) => ({
+      id: data.branch.id,
+      name: data.branch.name,
+      description: data.branch.description,
+      updatedAt: data.branch.updatedAt ?? data.branch.createdAt,
+      projectId: data.branch.projectId,
+      projectName: data.project?.name,
+      projectPicture: data.project?.picture,
+    })),
+    posts: posts.map((data) => ({
+      id: data.post.id,
+      title: data.post.title,
+      content: data.post.content,
+      updatedAt: data.post.updatedAt ?? data.post.createdAt,
+      media: data.media,
     })),
   };
 }
