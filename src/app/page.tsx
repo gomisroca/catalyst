ef17f85e57f4ef6a6239bbfd8e80f34c4907a18c
asfalfa <glkin9@gmail.com>
2025-03-14 18:01:31 +0100

    chore: cleanup home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <p className="text-center text-2xl">
-        {session && (
-          <span>Logged in as {session.user?.name ? session.user?.name : session.user?.email.split('@')[0]}</span>
-        )}
-      </p>
+      {/* 
+      If you are logged in, you get a timeline of the stuff the people you follow have done.
+      This should also include things happening in projects/branches you have contributed to. (Probably need an option to hide this per project/branch)
+
+      Whether you are logged in or not, you get a list of popular projects, branches and posts.
+      */}
     </main>
   );
 }
