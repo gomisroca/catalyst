de1ae6ac2df5bc8daa1dd89cbf1d642867252b83
asfalfa <glkin9@gmail.com>
2025-03-15 16:15:32 +0100

    chore: cleanup home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import { auth } from '@/server/auth';
+import HomeTimelineWrapper from './_components/home/timeline-wrapper';
 
 export default async function Home() {
   const session = await auth();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      {/* 
-      If you are logged in, you get a timeline of the stuff the people you follow have done.
-      This should also include things happening in projects/branches you have contributed to. (Probably need an option to hide this per project/branch)
-
-      Whether you are logged in or not, you get a list of popular projects, branches and posts.
-      */}
+    <main className="flex min-h-screen flex-col items-center justify-start">
+      <HomeTimelineWrapper session={session} />
     </main>
   );
 }
