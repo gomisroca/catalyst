89e0dc7ea71ecb5ab15a6b072c982837c8923388
asfalfa <glkin9@gmail.com>
2025-03-17 18:13:15 +0100

    feat: init basic FY timeline query

--- src/server/queries/timelines.ts
+++ src/server/queries/timelines.ts
@@ -1,7 +1,8 @@
-import { eq, sql } from 'drizzle-orm';
+import { eq, inArray, sql } from 'drizzle-orm';
 import { db } from '../db';
-import { projects, projectsInteractions } from '../db/schema';
+import { branches, branchesInteractions, posts, postsInteractions, projects, projectsInteractions } from '../db/schema';
 import { auth } from '../auth';
+import { getUserFollows } from './users';
 
 export async function getTrendingTimeline() {
   const sortedProjects = await db
@@ -36,5 +37,63 @@ export async function getForYouTimeline() {
   const session = await auth();
   if (!session) throw new Error('Not authenticated');
 
-  return [];
+  const follows = await getUserFollows(session.user.id);
+  const followsIds = follows.map((follow) => follow.followedId);
+
+  const [userPostInteractions, userBranchInteractions, userProjectInteractions] = await Promise.all([
+    await db
+      .select()
+      .from(postsInteractions)
+      .where(inArray(postsInteractions.userId, followsIds))
+      .orderBy(sql`${postsInteractions.createdAt} DESC`),
+    await db
+      .select()
+      .from(branchesInteractions)
+      .where(inArray(branchesInteractions.userId, followsIds))
+      .orderBy(sql`${branchesInteractions.createdAt} DESC`),
+    await db
+      .select()
+      .from(projectsInteractions)
+      .where(inArray(projectsInteractions.userId, followsIds))
+      .orderBy(sql`${projectsInteractions.createdAt} DESC`),
+  ]);
+
+  const [userPosts, userBranches, userProjects] = await Promise.all([
+    await db
+      .select()
+      .from(posts)
+      .where(inArray(posts.authorId, followsIds))
+      .orderBy(sql`${posts.updatedAt} DESC`),
+    await db
+      .select()
+      .from(branches)
+      .where(inArray(branches.authorId, followsIds))
+      .orderBy(sql`${branches.updatedAt} DESC`),
+    await db
+      .select()
+      .from(projects)
+      .where(inArray(projects.authorId, followsIds))
+      .orderBy(sql`${projects.updatedAt} DESC`),
+  ]);
+
+  return [
+    ...userPostInteractions.map((interaction) => ({
+      ...interaction,
+      type: 'postInteraction',
+      timestamp: interaction.createdAt,
+    })),
+    ...userBranchInteractions.map((interaction) => ({
+      ...interaction,
+      type: 'branchInteraction',
+      timestamp: interaction.createdAt,
+    })),
+    ...userProjectInteractions.map((interaction) => ({
+      ...interaction,
+      type: 'projectInteraction',
+      timestamp: interaction.createdAt,
+    })),
+    ...userPosts.map((post) => ({ ...post, type: 'post', timestamp: post.createdAt })),
+    ...userBranches.map((branch) => ({ ...branch, type: 'branch', timestamp: branch.createdAt })),
+    ...userProjects.map((project) => ({ ...project, type: 'project', timestamp: project.createdAt })),
+  ];
 }
