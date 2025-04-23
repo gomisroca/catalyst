2581abb3275bb81c3dd34fc3b3d67fdd385ec940
asfalfa <glkin9@gmail.com>
2025-04-23 21:29:59 +0200

    feat: add profilePicture FileRoute to uploadThing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,7 +6,37 @@ const f = createUploadthing();
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const UploadThingRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
+  profilePicture: f({
+    image: {
+      /**
+       * For full list of options and defaults, see the File Route API reference
+       * @see https://docs.uploadthing.com/file-routes#route-config
+       */
+      maxFileSize: '2MB',
+      maxFileCount: 1,
+    },
+  })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const session = await auth();
+
+      // If you throw, the user will not be able to upload
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
+      if (!session?.user) throw new UploadThingError('Unauthorized');
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: session.user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log('Upload complete for userId:', metadata.userId);
+
+      console.log('file url', file.ufsUrl);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId, url: file.ufsUrl };
+    }),
   projectPicture: f({
     image: {
       /**
