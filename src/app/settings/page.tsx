6558cc1eaf9ec76e04e0c8793a1f775dff65d89c
asfalfa <glkin9@gmail.com>
2025-04-24 09:24:20 +0200

    style: adjust signin link

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,8 @@
 import { Suspense } from 'react';
 import UserSettingsForm from './settings-form';
-import { LoadingSpinner } from '../_components/loading-spinner';
+import { LoadingSpinner } from '@/app/_components/loading-spinner';
 import { auth } from '@/server/auth';
+import Link from '@/app/_components/ui/link';
 
 export default async function UserSettings() {
   const session = await auth();
@@ -9,7 +10,10 @@ export default async function UserSettings() {
     return (
       <div className="flex flex-col gap-4">
         <p>You need to be logged in to see change your settings.</p>
-        <a href="/sign-in">Login</a>
+
+        <Link href="/sign-in" className="mx-auto w-1/2 text-center">
+          Login
+        </Link>
       </div>
     );
 
