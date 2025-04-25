b8ef9a84778e100be87b6f575cfa4c9a62ac8593
asfalfa <glkin9@gmail.com>
2025-04-25 11:32:54 +0200

    fix: missing submit btn and current user img if available

--- src/app/settings/settings-form.tsx
+++ src/app/settings/settings-form.tsx
@@ -7,10 +7,10 @@ import Form from 'next/form';
 import { useRef, useState } from 'react';
 import { updateUserSettings } from './actions';
 import { type User } from 'next-auth';
+import Image from 'next/image';
+import SubmitButton from '../_components/submit-button';
 
 export default function UserSettingsForm({ user }: { user: User }) {
-  console.log(user);
-
   const [file, setFile] = useState<File | null>(null);
   const setMessage = useSetAtom(messageAtom);
   const formRef = useRef<HTMLFormElement>(null);
@@ -49,6 +49,7 @@ export default function UserSettingsForm({ user }: { user: User }) {
           className="rounded-lg border-2 border-zinc-300 bg-zinc-200 p-2 focus:ring-2 focus:ring-sky-300 focus:outline-none dark:border-zinc-700 dark:bg-zinc-800 dark:focus:ring-sky-700"
           type="text"
           name="name"
+          defaultValue={user.name ?? ''}
           placeholder={user.name ?? 'Your Username'}
         />
       </section>
@@ -59,12 +60,25 @@ export default function UserSettingsForm({ user }: { user: User }) {
           className="rounded-lg border-2 border-zinc-300 bg-zinc-200 p-2 focus:ring-2 focus:ring-sky-300 focus:outline-none dark:border-zinc-700 dark:bg-zinc-800 dark:focus:ring-sky-700"
           type="email"
           name="email"
+          defaultValue={user.email ?? ''}
           placeholder={user.email ?? 'Your Email'}
         />
       </section>
 
-      <section className="flex w-full flex-col">
+      <section className="flex w-full flex-col gap-2">
         <label htmlFor="imageFile">Avatar</label>
+        {user.image && (
+          <div className="mx-auto flex flex-col text-center">
+            <h4>Current Avatar</h4>
+            <Image
+              className="rounded-lg border-2 border-zinc-300 bg-zinc-200 focus:ring-2 focus:ring-sky-300 focus:outline-none dark:border-zinc-700 dark:bg-zinc-800 dark:focus:ring-sky-700"
+              src={user.image}
+              height={200}
+              width={200}
+              alt="Avatar"
+            />
+          </div>
+        )}
         <input
           className="rounded-lg border-2 border-zinc-300 bg-zinc-200 file:mr-4 file:bg-zinc-300 file:p-2 file:transition file:duration-200 file:hover:bg-zinc-400 focus:ring-2 focus:ring-sky-300 focus:outline-none dark:border-zinc-700 dark:bg-zinc-800 dark:file:bg-zinc-700 dark:file:hover:bg-zinc-600 dark:focus:ring-sky-700"
           type="file"
@@ -77,6 +91,8 @@ export default function UserSettingsForm({ user }: { user: User }) {
           multiple={false}
         />
       </section>
+
+      <SubmitButton baseText="Update" pendingText="Updating..." className="w-full" />
     </Form>
   );
 }
