c51fdd715cb84e1cba74da05d5793a65df11b881
asfalfa <glkin9@gmail.com>
2025-04-28 15:40:17 +0200

    chore: typos

--- src/app/@modal/(.)settings/page.tsx
+++ src/app/@modal/(.)settings/page.tsx
@@ -3,7 +3,7 @@ import Modal from '@/app/_components/ui/modal';
 import UserSettingsForm from '@/app/settings/settings-form';
 import { auth } from '@/server/auth';
 
-export default async function CreateProjectModal() {
+export default async function UserSettingsModal() {
   const session = await auth();
   if (!session)
     return (
