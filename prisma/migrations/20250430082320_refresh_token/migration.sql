f0345c0da3e8df63b97a8cc598ef2d26eadc3ce1
asfalfa <glkin9@gmail.com>
2025-04-30 18:26:12 +0200

    fix: adjust models to fit Prisma

---
+++ prisma/migrations/20250430082320_refresh_token/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `refresh_token` on the `Account` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "refresh_token",
+ADD COLUMN     "refreshToken" TEXT;
