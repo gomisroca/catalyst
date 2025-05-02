2969f45aa7ed7559759c917f54f5af419fb61f94
asfalfa <glkin9@gmail.com>
2025-05-02 12:46:16 +0200

    fix: redirect hook to handle redirects within modal

---
+++ src/hooks/useRedirect.ts
@@ -0,0 +1,18 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+export function useRedirect() {
+  const router = useRouter();
+
+  return (modal = false, to: string, delay = 200) => {
+    if (modal) {
+      router.back();
+      setTimeout(() => {
+        router.replace(to);
+      }, delay);
+    } else {
+      router.push(to);
+    }
+  };
+}
