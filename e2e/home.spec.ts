5c16be13e87ee439c86ccfef7d87dd3b87163612
Adrià Gomis <gomisroca@gmail.com>
2025-07-07 14:39:38 +0200

    feat: home should have timeline

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
@@ -31,4 +31,17 @@ test('has a search function', async ({ page }) => {
 
   await expect(page.getByRole('textbox', { name: 'Search' })).toBeVisible();
   await expect(page.getByRole('button', { name: 'Go to Search' })).toBeVisible();
+});
+
+test('has a timeline', async ({ page }) => {
+  await page.goto('/');
+
+  const projectLink = page.getByRole('listitem').first();
+  await expect(projectLink).toBeInViewport();
+  const projectName = projectLink.getByRole('link').first();
+  const href = await projectName.getAttribute('href');
+
+  await projectName.click();
+
+  await expect(page).toHaveURL(href!);
 });
