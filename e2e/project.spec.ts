3a18438a593b5dc04ce501380544c45c7d424522
Adrià Gomis <gomisroca@gmail.com>
2025-07-07 15:17:12 +0200

    feat: create mock project to test page

--- e2e/project.spec.ts
+++ e2e/project.spec.ts
@@ -1,18 +1,44 @@
 import { expect, test } from '@playwright/test';
 
-test('has project name', async ({ page }) => {
-  await page.goto('/');
+import { db } from '@/server/db';
 
-  const projectLink = page.getByRole('listitem').first();
-  await expect(projectLink).toBeInViewport();
+test('has project details', async ({ page }) => {
+  const project = await db.$transaction(async (trx) => {
+    const newProject = await trx.project.create({
+      data: {
+        name: 'Temp Project',
+        description: 'This is a temporary project',
+        authorId: 'c3895654-8595-46cf-9190-a89ff1ce8750',
+      },
+      include: {
+        author: true,
+      },
+    });
+    await trx.projectPermissions.create({
+      data: {
+        projectId: newProject.id,
+        private: false,
+        allowCollaborate: false,
+        allowShare: false,
+      },
+    });
 
-  const projectName = projectLink.getByRole('link').first();
-  const href = await projectName.getAttribute('href');
-  const name = await projectName.innerText();
+    return newProject;
+  });
 
-  await projectName.click();
+  await page.goto('/projects/' + project.id);
 
-  await expect(page).toHaveURL(href!);
+  await expect(page).toHaveURL('/projects/' + project.id);
+  await expect(page.getByRole('heading', { name: project.name })).toBeVisible();
+  await expect(
+    page.getByRole('link', { name: project.author.name ?? project.author.email.split('@')[0] })
+  ).toBeVisible();
+  await expect(page.getByText('This is a temporary project')).toBeVisible();
 
-  await expect(page.getByRole('heading', { name })).not.toBeEmpty();
+  // Clean up
+  await db.project.delete({
+    where: {
+      id: project.id,
+    },
+  });
 });
