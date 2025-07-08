a48802dbd285be336d6ca2629f71e0bd89a04b67
Adrià Gomis <gomisroca@gmail.com>
2025-07-08 12:16:07 +0200

    feat: project delete on test finalization

--- e2e/project.spec.ts
+++ e2e/project.spec.ts
@@ -26,19 +26,22 @@ test('has project details', async ({ page }) => {
     return newProject;
   });
 
-  await page.goto('/projects/' + project.id);
+  try {
+    await page.goto('/projects/' + project.id);
 
-  await expect(page).toHaveURL('/projects/' + project.id);
-  await expect(page.getByRole('heading', { name: project.name })).toBeVisible();
-  await expect(
-    page.getByRole('link', { name: project.author.name ?? project.author.email.split('@')[0] })
-  ).toBeVisible();
-  await expect(page.getByText('This is a temporary project')).toBeVisible();
-
-  // Clean up
-  await db.project.delete({
-    where: {
-      id: project.id,
-    },
-  });
+    await expect(page).toHaveURL('/projects/' + project.id);
+    await expect(page.getByRole('heading', { name: project.name })).toBeVisible();
+    await expect(
+      page.getByRole('link', {
+        name: project.author.name ?? project.author.email.split('@')[0],
+      })
+    ).toBeVisible();
+    await expect(page.getByText('This is a temporary project')).toBeVisible();
+  } finally {
+    await db.project.delete({
+      where: {
+        id: project.id,
+      },
+    });
+  }
 });
