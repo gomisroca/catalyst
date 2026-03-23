a1bf1cab1d9723b3ea38116ed74fa5b6c38e7985
Adrià Gomis <gomisroca@gmail.com>
2026-03-23 15:54:58 +0100

    style: update tabs slightly to match new styling

--- src/app/_components/home/tabs.tsx
+++ src/app/_components/home/tabs.tsx
@@ -15,12 +15,12 @@ function TabButton({ tab, text, currentTab, index }: { tab: string; text: string
     <Link
       href={tab === 'trending' ? pathname : pathname + '?tl=' + tab}
       className={twMerge(
-        'cursor-pointer bg-zinc-300 p-2 text-sm font-bold uppercase drop-shadow-sm transition-all duration-200 ease-in-out dark:bg-zinc-800',
-        currentTab === tab && 'pointer-events-none bg-zinc-400 dark:bg-zinc-900',
-        currentTab !== tab &&
-          'hover:scale- hover:bg-white hover:drop-shadow-md active:scale-90 active:duration-100 dark:hover:bg-black',
+        'cursor-pointer px-4 py-2 text-sm font-bold uppercase drop-shadow-sm transition-all duration-200 ease-in-out',
         index === 0 && 'rounded-l-lg',
-        index === 1 && 'rounded-r-lg'
+        index === 1 && 'rounded-r-lg',
+        currentTab === tab
+          ? 'pointer-events-none bg-white shadow-inner dark:bg-black'
+          : 'bg-zinc-300 hover:bg-white hover:drop-shadow-md active:scale-90 active:duration-100 dark:bg-zinc-800 dark:hover:bg-black'
       )}>
       {text}
     </Link>
@@ -30,7 +30,6 @@ function TabButton({ tab, text, currentTab, index }: { tab: string; text: string
 export default function HomeTabs({ tab }: { tab: string }) {
   return (
     <div className="mb-4 flex items-center rounded-lg bg-zinc-300 dark:bg-zinc-800">
-      {/* Render the "Trending" and "For You" tabs */}
       <TabButton tab="trending" currentTab={tab} index={0} text="Trending" />
       <TabButton tab="for-you" currentTab={tab} index={1} text="For You" />
     </div>
