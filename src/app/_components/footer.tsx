4161bafd58b84d97c4cd7de4a9135405e0d49b49
Adrià Gomis <gomisroca@gmail.com>
2026-03-24 12:39:18 +0100

    style: lower contrast  footer text

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -10,7 +10,9 @@ function Footer() {
     <>
       <Message />
       <footer className="fixed bottom-0 flex h-12 w-full items-center justify-between space-x-2 px-4">
-        <p className="text-sm leading-none tracking-tight">© {new Date().getFullYear()} Catalyst</p>
+        <p className="text-sm leading-none tracking-tight text-zinc-400 dark:text-zinc-800">
+          © {new Date().getFullYear()} Catalyst
+        </p>
         <ThemeButton />
       </footer>
     </>
