66c93434a30a02ff38393b55e20407530cfe722c
Adrià Gomis <gomisroca@gmail.com>
2026-03-24 12:28:40 +0100

    style: update cards to match new styling

--- src/app/_components/cards.tsx
+++ src/app/_components/cards.tsx
@@ -27,7 +27,7 @@ export function ProjectCard({ project }: { project: ExtendedProject }) {
   return (
     <li
       key={project.id}
-      className="group flex max-w-full rounded-lg bg-zinc-300 drop-shadow-sm transition duration-200 ease-in-out hover:scale-105 hover:drop-shadow-md active:drop-shadow-none active:duration-100 dark:bg-zinc-950">
+      className="group flex h-30 max-w-full rounded-lg bg-zinc-300 drop-shadow-sm transition-all duration-200 ease-in-out hover:bg-white hover:drop-shadow-md active:scale-95 active:duration-100 dark:bg-zinc-800 dark:hover:bg-black">
       {project.picture && (
         <Image
           src={project.picture}
@@ -38,17 +38,17 @@ export function ProjectCard({ project }: { project: ExtendedProject }) {
         />
       )}
       <section className="flex h-full w-full flex-col">
-        <header className="flex flex-col items-start justify-between gap-1 rounded-tr-lg bg-white px-2 py-2 transition duration-200 ease-in-out md:flex-row md:items-center md:px-4 dark:bg-black">
+        <header className="flex flex-col items-start justify-between gap-1 rounded-tr-lg bg-white/50 px-2 py-2 transition duration-200 ease-in-out md:flex-row md:items-center md:px-4 dark:bg-black/30">
           <Link
             href={`/projects/${project.id}/`}
-            className="w-full leading-3 font-bold transition duration-200 ease-in-out hover:scale-105 hover:text-rose-500 md:w-auto md:text-lg dark:hover:text-rose-700">
+            className="w-full leading-3 font-bold transition duration-200 ease-in-out hover:underline md:w-auto md:text-lg">
             {project.name}
           </Link>
           <p className="w-full text-sm leading-3 text-zinc-400 md:w-auto md:text-base">
             {formatDate(project.updatedAt!)}
           </p>
         </header>
-        <div className="my-2 line-clamp-5 px-2 md:px-4">{project.description}</div>
+        <div className="my-2 line-clamp-3 flex-1 px-2 md:px-4">{project.description}</div>
       </section>
     </li>
   );
@@ -58,7 +58,7 @@ export function BranchCard({ branch }: { branch: ExtendedBranch }) {
   return (
     <li
       key={branch.id}
-      className="group flex max-w-full rounded-lg bg-zinc-300 drop-shadow-sm transition duration-200 ease-in-out hover:scale-105 hover:drop-shadow-md active:drop-shadow-none active:duration-100 dark:bg-zinc-950">
+      className="group flex h-30 max-w-full rounded-lg bg-zinc-300 drop-shadow-sm transition-all duration-200 ease-in-out hover:bg-white hover:drop-shadow-md active:scale-95 active:duration-100 dark:bg-zinc-800 dark:hover:bg-black">
       {branch.project.picture && (
         <Image
           src={branch.project.picture}
@@ -69,17 +69,17 @@ export function BranchCard({ branch }: { branch: ExtendedBranch }) {
         />
       )}
       <section className="flex h-full w-full flex-col">
-        <header className="flex flex-col items-center justify-between gap-1 rounded-tr-lg bg-white px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black">
+        <header className="flex flex-col items-center justify-between gap-1 rounded-tr-lg bg-white/50 px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black/30">
           <div className="flex w-full flex-col items-start gap-1 md:w-auto md:flex-row md:items-center md:gap-2">
             <Link
               href={`/projects/${branch.projectId}/`}
-              className="text-sm leading-3 font-bold transition duration-200 ease-in-out hover:scale-105 hover:text-rose-500 md:text-base dark:hover:text-rose-700">
+              className="text-sm leading-3 font-bold transition duration-200 ease-in-out hover:underline md:text-base">
               {branch.project.name}
             </Link>
             <span className="hidden leading-3 md:block">•</span>
             <Link
               href={`/projects/${branch.projectId}/${branch.id}`}
-              className="flex items-center gap-1 leading-3 font-bold text-zinc-500 transition duration-200 ease-in-out hover:scale-105 hover:text-rose-500 dark:hover:text-rose-700">
+              className="flex items-center gap-1 leading-3 font-bold text-zinc-500 transition duration-200 ease-in-out hover:underline">
               <FaCodeBranch size={14} /> {branch.name}
             </Link>
           </div>
@@ -87,7 +87,7 @@ export function BranchCard({ branch }: { branch: ExtendedBranch }) {
             {formatDate(branch.updatedAt!)}
           </p>
         </header>
-        <div className="my-2 line-clamp-5 px-2 md:px-4">{branch.description}</div>
+        <div className="my-2 line-clamp-3 flex-1 px-2 md:px-4">{branch.description}</div>
       </section>
     </li>
   );
@@ -97,18 +97,18 @@ export function PostCard({ post }: { post: ExtendedPost }) {
   return (
     <li
       key={post.id}
-      className="group flex max-w-full flex-col rounded-lg bg-zinc-300 drop-shadow-sm transition duration-200 ease-in-out hover:scale-105 hover:drop-shadow-md active:drop-shadow-none active:duration-100 dark:bg-zinc-950">
-      <header className="flex flex-col items-center justify-between rounded-t-lg bg-white px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black">
+      className="group flex h-30 max-w-full flex-col rounded-lg bg-zinc-300 drop-shadow-sm transition-all duration-200 ease-in-out hover:bg-white hover:drop-shadow-md active:scale-95 active:duration-100 dark:bg-zinc-800 dark:hover:bg-black">
+      <header className="flex flex-col items-center justify-between rounded-t-lg bg-white/50 px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black/30">
         <div className="flex w-full flex-col items-start text-sm md:w-auto md:flex-row md:items-center md:gap-2 md:text-base">
           <Link
             href={`/projects/${post.branch.projectId}`}
-            className="text-sm leading-3 font-bold transition duration-200 ease-in-out hover:scale-105 hover:text-rose-500 md:text-base dark:hover:text-rose-700">
+            className="text-sm leading-3 font-bold transition duration-200 ease-in-out hover:underline md:text-base">
             {post.branch.project.name}
           </Link>
           <span className="hidden leading-3 md:block">•</span>
           <Link
             href={`/projects/${post.branch.projectId}/${post.branch.id}`}
-            className="flex items-center gap-1 text-sm leading-3 font-bold text-zinc-500 transition duration-200 ease-in-out hover:scale-105 hover:text-rose-500 md:text-base dark:hover:text-rose-700">
+            className="flex items-center gap-1 text-sm leading-3 font-bold text-zinc-500 transition duration-200 ease-in-out hover:underline md:text-base">
             <FaCodeBranch size={14} /> {post.branch.name}
           </Link>
         </div>
@@ -116,9 +116,9 @@ export function PostCard({ post }: { post: ExtendedPost }) {
           {post.updatedAt!.toLocaleDateString()}
         </p>
       </header>
-      <section className="my-2 flex flex-col gap-2 px-2 md:px-4">
+      <section className="my-2 flex flex-1 flex-col gap-2 px-2 md:px-4">
         <h3 className="text-xl leading-3 font-semibold">{post.title}</h3>
-        <p className="line-clamp-5 text-zinc-500">{post.content}</p>
+        <p className="line-clamp-3 text-zinc-500">{post.content}</p>
         <div className="flex flex-wrap gap-2">
           {post.media.map((media) => (
             <Image
@@ -157,8 +157,8 @@ export function ProjectInteractionCard({ interaction }: { interaction: ExtendedP
   return (
     <li
       key={interaction.id}
-      className="group flex max-w-full flex-col rounded-lg bg-zinc-300 drop-shadow-sm transition duration-200 ease-in-out hover:scale-105 hover:drop-shadow-md active:drop-shadow-none active:duration-100 dark:bg-zinc-950">
-      <header className="flex flex-col items-center justify-between rounded-t-lg bg-white px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black">
+      className="group flex h-30 max-w-full flex-col rounded-lg bg-zinc-300 drop-shadow-sm transition-all duration-200 ease-in-out hover:bg-white hover:drop-shadow-md active:scale-95 active:duration-100 dark:bg-zinc-800 dark:hover:bg-black">
+      <header className="flex flex-col items-center justify-between rounded-t-lg bg-white/50 px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black/30">
         <p className="flex w-full items-center justify-start gap-1 font-semibold text-zinc-500">
           {interactionIcons[type].icon} {interactionIcons[type].text} a project
         </p>
@@ -171,12 +171,12 @@ export function ProjectInteractionCard({ interaction }: { interaction: ExtendedP
             <span className="font-normal">by</span>
             <Link
               href={`/profile/${interaction.user.id}`}
-              className="transition duration-200 ease-in-out hover:text-zinc-600">
+              className="transition duration-200 ease-in-out hover:underline">
               {interaction.user.name ?? interaction.user.email.split('@')[0]}
             </Link>
           </div>
         </div>
-        <p className="line-clamp-5 text-zinc-500">{interaction.project.description}</p>
+        <p className="line-clamp-2 text-zinc-500">{interaction.project.description}</p>
       </section>
     </li>
   );
@@ -187,8 +187,8 @@ export function BranchInteractionCard({ interaction }: { interaction: ExtendedBr
   return (
     <li
       key={interaction.id}
-      className="group flex max-w-full flex-col rounded-lg bg-zinc-300 drop-shadow-sm transition duration-200 ease-in-out hover:scale-105 hover:drop-shadow-md active:drop-shadow-none active:duration-100 dark:bg-zinc-950">
-      <header className="flex flex-col items-center justify-between rounded-t-lg bg-white px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black">
+      className="group flex h-30 max-w-full flex-col rounded-lg bg-zinc-300 drop-shadow-sm transition-all duration-200 ease-in-out hover:bg-white hover:drop-shadow-md active:scale-95 active:duration-100 dark:bg-zinc-800 dark:hover:bg-black">
+      <header className="flex flex-col items-center justify-between rounded-t-lg bg-white/50 px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black/30">
         <p className="flex w-full items-center justify-start gap-1 font-semibold text-zinc-500">
           {interactionIcons[type].icon} {interactionIcons[type].text} a branch
         </p>
@@ -201,12 +201,12 @@ export function BranchInteractionCard({ interaction }: { interaction: ExtendedBr
             <span className="font-normal">by</span>
             <Link
               href={`/profile/${interaction.user.id}`}
-              className="transition duration-200 ease-in-out hover:text-zinc-600">
+              className="transition duration-200 ease-in-out hover:underline">
               {interaction.user.name ?? interaction.user.email.split('@')[0]}
             </Link>
           </div>
         </div>
-        <p className="line-clamp-5 text-zinc-500">{interaction.branch.description}</p>
+        <p className="line-clamp-2 text-zinc-500">{interaction.branch.description}</p>
       </section>
     </li>
   );
@@ -217,8 +217,8 @@ export function PostInteractionCard({ interaction }: { interaction: ExtendedPost
   return (
     <li
       key={interaction.id}
-      className="group flex max-w-full flex-col rounded-lg bg-zinc-300 drop-shadow-sm transition duration-200 ease-in-out hover:scale-105 hover:drop-shadow-md active:drop-shadow-none active:duration-100 dark:bg-zinc-950">
-      <header className="flex flex-col items-center justify-between rounded-t-lg bg-white px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black">
+      className="group flex h-30 max-w-full flex-col rounded-lg bg-zinc-300 drop-shadow-sm transition-all duration-200 ease-in-out hover:bg-white hover:drop-shadow-md active:scale-95 active:duration-100 dark:bg-zinc-800 dark:hover:bg-black">
+      <header className="flex flex-col items-center justify-between rounded-t-lg bg-white/50 px-2 py-2 transition duration-200 ease-in-out md:flex-row md:px-4 dark:bg-black/30">
         <p className="flex w-full items-center justify-start gap-1 font-semibold text-zinc-500">
           {interactionIcons[type].icon} {interactionIcons[type].text} a post
         </p>
@@ -231,21 +231,12 @@ export function PostInteractionCard({ interaction }: { interaction: ExtendedPost
             <span className="font-normal">by</span>
             <Link
               href={`/profile/${interaction.user.id}`}
-              className="transition duration-200 ease-in-out hover:text-zinc-600">
+              className="transition duration-200 ease-in-out hover:underline">
               {interaction.user.name ?? interaction.user.email.split('@')[0]}
             </Link>
           </div>
         </div>
-        <p className="line-clamp-5 text-zinc-500">{interaction.post.content}</p>
-        {interaction.post.media && (
-          <div className="flex flex-wrap">
-            {interaction.post.media.map((media) => (
-              <div key={media.id} className="h-10 w-10 p-2">
-                <Image fill src={media.url} alt={media.name} />
-              </div>
-            ))}
-          </div>
-        )}
+        <p className="line-clamp-2 text-zinc-500">{interaction.post.content}</p>
       </section>
     </li>
   );
