3ff2e1e00f0076c15cf0fa2ceef0471a85990096
Adrià Gomis <gomisroca@gmail.com>
2026-03-30 13:01:31 +0200

    feat: cleanup and use dompurify for post.content

--- src/app/projects/[projectId]/(branches)/[branchId]/post-list.tsx
+++ src/app/projects/[projectId]/(branches)/[branchId]/post-list.tsx
@@ -1,37 +1,43 @@
 import { type Prisma } from 'generated/prisma';
+import DOMPurify from 'isomorphic-dompurify';
 import Image from 'next/image';
 import { type Session } from 'next-auth';
 
 import AuthorActions from '@/app/_components/projects/author-actions';
-import PostInteractionsMenu from '@/app/projects/[projectId]/(branches)/[branchId]/(posts)/(interactions)/post-interactions-menu';
+
+import PostInteractionsMenu from './(posts)/(interactions)/post-interaction';
 
 type ExtendedPost = Prisma.PostGetPayload<{
   include: { media: true; author: true; interactions: { include: { user: true } } };
 }>;
 
-function Post({ post, session }: { post: ExtendedPost; session: Session | null }) {
+function Post({
+  post,
+  session,
+  projectId,
+  branchId,
+}: {
+  post: ExtendedPost;
+  session: Session | null;
+  projectId: string;
+  branchId: string;
+}) {
   return (
-    <div
-      key={post.id}
-      className="group flex flex-col rounded-lg bg-zinc-200 drop-shadow-sm hover:drop-shadow-md dark:bg-zinc-800">
-      <div className="spy-2 flex items-center justify-between rounded-t-lg bg-zinc-100 p-2 text-lg font-bold transition duration-200 ease-in-out group-hover:bg-zinc-300 dark:bg-zinc-900 group-hover:dark:bg-zinc-950">
+    <div className="group flex flex-col rounded-lg bg-zinc-200 drop-shadow-sm hover:drop-shadow-md dark:bg-zinc-800">
+      <div className="flex items-center justify-between rounded-t-lg bg-zinc-300 p-2 transition duration-200 ease-in-out group-hover:bg-white dark:bg-zinc-700 dark:group-hover:bg-black">
         <h1 className="text-lg font-bold">{post.title}</h1>
         <div className="flex items-center gap-1 text-sm font-medium text-zinc-500 dark:text-zinc-400">
           <span>{post.author.name ?? post.author.email.split('@')[0]}</span>
           <span>•</span>
-          <span>
-            {post.createdAt.toLocaleDateString('en-US', {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-            })}
-          </span>
+          <span>{post.createdAt.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</span>
           {session?.user.id === post.author.id && (
             <AuthorActions type="post" projectId={post.projectId} branchId={post.branchId} postId={post.id} />
           )}
         </div>
       </div>
-      <div className="flex-1 p-4" dangerouslySetInnerHTML={{ __html: post.content! }} />
+
+      <div className="flex-1 p-4" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content ?? '') }} />
+
       {post.media.length > 0 && (
         <section className="flex flex-wrap gap-2 p-2">
           {post.media.map((media) => (
@@ -46,18 +52,31 @@ function Post({ post, session }: { post: ExtendedPost; session: Session | null }
           ))}
         </section>
       )}
-      <section className="flex items-center justify-between rounded-b-lg bg-zinc-100 p-2 text-sm font-medium transition duration-200 ease-in-out group-hover:bg-zinc-300 dark:bg-zinc-900 group-hover:dark:bg-zinc-950">
-        <PostInteractionsMenu postId={post.id} />
+
+      <section className="flex items-center justify-between rounded-b-lg bg-zinc-300 p-2 text-sm font-medium transition duration-200 ease-in-out group-hover:bg-white dark:bg-zinc-700 dark:group-hover:bg-black">
+        <PostInteractionsMenu projectId={projectId} branchId={branchId} postId={post.id} />
       </section>
     </div>
   );
 }
 
-export default async function PostList({ posts, session }: { posts: ExtendedPost[]; session: Session | null }) {
+export default function PostList({
+  posts,
+  session,
+  projectId,
+  branchId,
+}: {
+  posts: ExtendedPost[];
+  session: Session | null;
+  projectId: string;
+  branchId: string;
+}) {
+  if (posts.length === 0) return <p className="text-center text-zinc-500">No posts yet.</p>;
+
   return (
     <div className="flex flex-col gap-4">
       {posts.map((post) => (
-        <Post key={post.id} post={post} session={session} />
+        <Post key={post.id} post={post} session={session} projectId={projectId} branchId={branchId} />
       ))}
     </div>
   );
