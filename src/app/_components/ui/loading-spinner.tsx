70c0670ff69da56bb749fed8eb81961246726006
Adrià Gomis <gomisroca@gmail.com>
2026-03-30 12:57:35 +0200

    fix: simplify loading spinner

--- src/app/_components/ui/loading-spinner.tsx
+++ src/app/_components/ui/loading-spinner.tsx
@@ -1,14 +1,5 @@
-/**
- * Loading spinner component with customizable size and color.
- * 
- * @example
- * <LoadingSpinner size="small" />
- */
-
-// Options for size and color
 interface LoadingSpinnerProps {
   size?: 'small' | 'medium' | 'large';
-  color?: string;
 }
 
 export default function LoadingSpinner({ size = 'medium' }: LoadingSpinnerProps) {
@@ -21,12 +12,10 @@ export default function LoadingSpinner({ size = 'medium' }: LoadingSpinnerProps)
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div
-        className={`${sizeClasses[size]} animate-spin rounded-full border-4 border-dotted border-rose-500 border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:border-rose-700 dark:border-r-transparent`}
+        className={`${sizeClasses[size]} animate-spin rounded-full border-4 border-zinc-300 border-t-zinc-800 dark:border-zinc-700 dark:border-t-zinc-100`}
         role="status"
         aria-label="Loading">
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !border-0 !p-0 !whitespace-nowrap ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
+        <span className="sr-only">Loading...</span>
       </div>
     </div>
   );
