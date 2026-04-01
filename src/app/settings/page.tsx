ac0fb9ac3ebe3ab0cb43ede26f1ba878cf5f0a85
Adrià Gomis <gomisroca@gmail.com>
2026-04-01 09:11:48 +0200

    fix: remove redundant suspense

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,21 +1,15 @@
-import { Suspense } from 'react';
-
 import NotAllowed from '@/app/_components/not-allowed';
-import LoadingSpinner from '@/app/_components/ui/loading-spinner';
 import UserSettingsForm from '@/app/settings/settings-form';
 import { auth } from '@/server/auth';
 
 export default async function UserSettings() {
   const session = await auth();
-  // If user is not logged in, show restricted access component
   if (!session) return <NotAllowed />;
 
   return (
     <>
       <h1 className="mb-4 text-xl font-bold">User Settings</h1>
-      <Suspense fallback={<LoadingSpinner />}>
-        <UserSettingsForm user={session.user} />
-      </Suspense>
+      <UserSettingsForm user={session.user} />
     </>
   );
 }
