f3e802d850338816ceda7fee726bfd3eb54c78c1
Adrià Gomis <gomisroca@gmail.com>
2026-04-18 10:38:28 +0200

    fix: client-side post interactions

--- src/app/projects/[projectId]/(branches)/[branchId]/(posts)/(interactions)/post-interaction.tsx
+++ src/app/projects/[projectId]/(branches)/[branchId]/(posts)/(interactions)/post-interaction.tsx
@@ -1,8 +1,11 @@
+'use client';
+
+import { type User } from 'next-auth';
+import { type InteractionWithUser } from 'types';
+
 import { togglePostInteraction } from '@/actions/posts';
 import ExtraInteractions from '@/app/_components/projects/extra-interactions';
 import InteractionButton from '@/app/_components/projects/interaction-button';
-import { auth } from '@/server/auth';
-import { getPostInteractions } from '@/server/queries/posts';
 
 const typeMap = {
   likes: 'LIKE',
@@ -10,33 +13,47 @@ const typeMap = {
   bookmarks: 'BOOKMARK',
 } as const;
 
-export default async function PostInteractionsMenu({
+type Interactions = {
+  likes: InteractionWithUser[];
+  shares: InteractionWithUser[];
+  bookmarks: InteractionWithUser[];
+};
+type ExtraInteractions = {
+  reports: InteractionWithUser[];
+  hides: InteractionWithUser[];
+};
+
+export default function PostInteractionsMenu({
   projectId,
   branchId,
   postId,
+  interactions,
+  extraInteractions,
+  user,
 }: {
   projectId: string;
   branchId: string;
   postId: string;
+  interactions: Interactions;
+  extraInteractions: ExtraInteractions;
+  user?: User;
 }) {
-  const [session, data] = await Promise.all([auth(), getPostInteractions(postId)]);
-
   return (
     <div className="flex flex-row gap-2">
-      {(Object.keys(data.interactions) as Array<keyof typeof data.interactions>).map((key) => (
+      {(Object.keys(interactions) as Array<keyof typeof interactions>).map((key) => (
         <InteractionButton
           key={key}
           type={typeMap[key]}
-          data={data.interactions[key]}
-          user={session?.user}
+          data={interactions[key]}
+          user={user}
           entityId={postId}
           entityKey="postId"
           onInteract={() => togglePostInteraction(typeMap[key], projectId, branchId, postId)}
         />
       ))}
       <ExtraInteractions
-        user={session?.user}
-        data={data.extraInteractions}
+        user={user}
+        data={extraInteractions}
         entityId={postId}
         entityKey="postId"
         onInteract={(type) => togglePostInteraction(type, projectId, branchId, postId)}
