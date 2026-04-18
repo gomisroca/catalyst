da3a224962ca6e1490f250534b9da8b6a7b9c785
Adrià Gomis <gomisroca@gmail.com>
2026-04-18 10:37:52 +0200

    fix: client-side project interactions

--- src/app/projects/[projectId]/(interactions)/project-interaction.tsx
+++ src/app/projects/[projectId]/(interactions)/project-interaction.tsx
@@ -1,34 +1,51 @@
+'use client';
+
+import { type User } from 'next-auth';
+import { type InteractionWithUser } from 'types';
+
 import { toggleProjectInteraction } from '@/actions/projects';
 import ExtraInteractions from '@/app/_components/projects/extra-interactions';
 import InteractionButton from '@/app/_components/projects/interaction-button';
-import { auth } from '@/server/auth';
-import { getProjectInteractions } from '@/server/queries/projects';
 
-const typeMap = {
-  likes: 'LIKE',
-  shares: 'SHARE',
-  bookmarks: 'BOOKMARK',
-} as const;
+const typeMap = { likes: 'LIKE', shares: 'SHARE', bookmarks: 'BOOKMARK' } as const;
 
-export default async function ProjectInteractionsMenu({ projectId }: { projectId: string }) {
-  const [session, data] = await Promise.all([auth(), getProjectInteractions(projectId)]);
+type Interactions = {
+  likes: InteractionWithUser[];
+  shares: InteractionWithUser[];
+  bookmarks: InteractionWithUser[];
+};
+type ExtraInteractions = {
+  reports: InteractionWithUser[];
+  hides: InteractionWithUser[];
+};
 
+export default function ProjectInteractionsMenu({
+  projectId,
+  interactions,
+  extraInteractions,
+  user,
+}: {
+  projectId: string;
+  interactions: Interactions;
+  extraInteractions: ExtraInteractions;
+  user?: User;
+}) {
   return (
     <div className="flex flex-row gap-2">
-      {(Object.keys(data.interactions) as Array<keyof typeof data.interactions>).map((key) => (
+      {(Object.keys(interactions) as Array<keyof typeof interactions>).map((key) => (
         <InteractionButton
           key={key}
           type={typeMap[key]}
-          data={data.interactions[key]}
-          user={session?.user}
+          data={interactions[key]}
+          user={user}
           entityId={projectId}
           entityKey="projectId"
           onInteract={() => toggleProjectInteraction(typeMap[key], projectId)}
         />
       ))}
       <ExtraInteractions
-        user={session?.user}
-        data={data.extraInteractions}
+        user={user}
+        data={extraInteractions}
         entityId={projectId}
         entityKey="projectId"
         onInteract={(type) => toggleProjectInteraction(type, projectId)}
